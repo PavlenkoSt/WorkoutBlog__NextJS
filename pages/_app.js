eebcee7047a53caf9c88020579c8e05c75cbfccb
Stanislav <verbatim8872@gmail.com>
2021-05-25 19:19:25 +0300

    add load progressbar and fix some styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
 import '../styles/globals.scss'
+import NextNprogress from 'nextjs-progressbar';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <NextNprogress
+        color="#29D"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height="3"
+        options={{ showSpinner: false }}
+      />
+    </>
+  )
+  
 }
 
 export default MyApp
