0a5105ba5efd216547771e4c85163d17517c3d46
Stanislav <verbatim8872@gmail.com>
2021-05-25 20:10:36 +0300

    add custom 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,17 @@
+import Image from "next/image";
+import MainLayout from "../components/layouts/MainLayout";
+
 export default function Error(){
     return (
-        <div>
-            404
-        </div>
+        <MainLayout title='404 Страница не найдена'>
+            <div style={{display: "flex", justifyContent: 'center', alignItems: 'center', padding: '30px 0'}}>
+                <Image
+                    width={500}
+                    height={400}
+                    alt='404 Страница не найдена'
+                    src='/404.webp'
+                />
+            </div>
+        </MainLayout>
     )
 }
