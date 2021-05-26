adaddc96cc42f588404e2f509f496fd89511ba90
Stanislav <verbatim8872@gmail.com>
2021-05-26 09:31:18 +0300

    add mobile adaptive to last articles and programs in main page

--- components/common/ArticlesListMain.jsx
+++ components/common/ArticlesListMain.jsx
@@ -6,12 +6,14 @@ export default function ArticlesListMain({ title, photo, id, date}){
     return (
         <Link href={`/articles/${id}`}>
             <a className={s.item}>
-                <Image
-                    src={photo}
-                    width={150}
-                    height={90}
-                    alt='article'
-                />
+                <div className={s.img}>
+                    <Image
+                        src={photo}
+                        width={150}
+                        height={90}
+                        alt='article'
+                    />
+                </div>
                 <div className={s.info}>
                     <span className={s.title}>{title}</span>
                     <span className={s.date}>{date}</span>
